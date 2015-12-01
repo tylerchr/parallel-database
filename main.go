0ecf8a6c2cd01360fea5b631a72ebc8fda18966f
Tyler Christensen <tyler9xp@gmail.com>
2015-12-01 08:26:20 -0700

    Wire in the query parser so we have a real REPL (also implemented min and max accumulators)

--- main.go
+++ main.go
@@ -5,16 +5,15 @@ import (
 	"bytes"
 	"time"
 	"reflect"
+	"bufio"
+	"os"
 
 	"github.com/boltdb/bolt"
 	"github.com/tylerchr/parallel-database/query"
+	"github.com/tylerchr/parallel-database/query/parser"
 )
 
 func main() {
-	doWork()
-}
-
-func doWork() {
 
 	db, err := bolt.Open("data.db", 0600, nil)
 	if err != nil {
@@ -23,23 +22,67 @@ func doWork() {
 	defer db.Close()
 
 	// start the timer
-	t0 := time.Now()
+	// t0 := time.Now()
 
-	query := query.Query{
-		Metrics: []query.QueryMetric{
-			query.QueryMetric{Column: "song_hotttnesss", Metric: "average"},
-		},
-		Filter: []query.QueryFilter{
-			// query.QueryFilter{Column: "title", Operator: "contains", Operand: "One"},
-			// query.QueryFilter{Column: "artist_location", Operator: "equals", Operand: "Detroit, MI"},
-		},
+	// query := query.Query{
+	// 	Metrics: []query.QueryMetric{
+	// 		query.QueryMetric{Column: "song_hotttnesss", Metric: "average"},
+	// 	},
+	// 	Filter: []query.QueryFilter{
+	// 		// query.QueryFilter{Column: "title", Operator: "contains", Operand: "One"},
+	// 		// query.QueryFilter{Column: "artist_location", Operator: "equals", Operand: "Detroit, MI"},
+	// 	},
+	// }
+
+	// executeQuery(db, query)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+
+			fmt.Print("> ")
+			text, _ := reader.ReadString('\n')
+
+			if text == "goodbye\n" {
+					fmt.Println("Goodbye!")
+					break
+			}
+
+			if q, err := parser.ParseQuery(text); err != nil {
+				fmt.Printf("[error] %v\n", err)
+			} else {
+				fmt.Printf("[parsed] %v\n", q)
+				t0 := time.Now()
+				executeQuery(db, q)
+				fmt.Printf("took %v\n", time.Now().Sub(t0))
+			}
+
 	}
+
+
+}
+
+func executeQuery(db *bolt.DB, q query.Query) {
 
 	// validate query
 	// make sure query is semantically valid
 	valid := true
 
 	_ = valid
+
+	accs := make([]Accumulator, len(q.Metrics))
+	for i, metric := range q.Metrics {
+		switch metric.Metric {
+		case "average":
+			accs[i] = &AverageAccumulator{Col: metric.Column}
+		case "count":
+			accs[i] = &CountAccumulator{Col: metric.Column}
+		case "min":
+			accs[i] = &MinAccumulator{Col: metric.Column}
+		case "max":
+			accs[i] = &MaxAccumulator{Col: metric.Column}
+		}
+	}
 
 	db.View(func(tx *bolt.Tx) error {
 		c := tx.Bucket([]byte("songs")).Cursor()
@@ -49,10 +92,10 @@ func doWork() {
 		songMap := make(map[string][]byte, 10)
 		var currentRecord []byte
 
-		accs := []Accumulator{
-			&AverageAccumulator{Col: "song_hotttnesss"},
-			&CountAccumulator{Col: "title"},
-		}
+		// accs := []Accumulator{
+		// 	&AverageAccumulator{Col: "song_hotttnesss"},
+		// 	&CountAccumulator{Col: "title"},
+		// }
 
 		finishedLastRow := false
 		for k, v := c.First(); k != nil || finishedLastRow == false; k, v = c.Next() {
@@ -62,7 +105,7 @@ func doWork() {
 				count += 1
 				if len(songMap) > 0 {
 
-					if passesFilters, err := evaluateFilters(query.Filter, songMap); err != nil {
+					if passesFilters, err := evaluateFilters(q.Filter, songMap); err != nil {
 						panic(err)
 					} else if passesFilters {
 
@@ -106,7 +149,6 @@ func doWork() {
 		for _, acc := range accs {
 			fmt.Printf("%#v\n", acc)
 		}
-		fmt.Printf("took %v\n", time.Now().Sub(t0))
 
 		return nil
 
--- main_test.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "testing"
-
-func BenchmarkStuff(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		doWork()
-	}
-}
